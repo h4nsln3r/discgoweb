903c66a10233a7d2aac1c50963988e07562e887b
Hannes Linnér <hanlin@netco.local>
2025-04-09 14:13:16 +0200

    Added rimraf to project to clean .next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 // src/app/layout.tsx
 import "../styles/global.scss";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Discgolf App",
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="sv">
-      <body className={inter.className}>
+      <body>
         <Navbar />
         {children}
       </body>
