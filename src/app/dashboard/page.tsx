0003a3cf5fefad355c30b0a02d71af4d12759103
h4nsln3r <h4nsln3r@gmail.com>
2025-07-26 23:59:45 +0200

    Added hannes ui no success jet

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,10 @@ import { createServerSupabaseClient } from "@/lib/supabase-server";
 import { redirect } from "next/navigation";
 import Map from "@/components/Map/Map";
 
+import { Badge } from "@biltema-ui/global";
+
+// import { Button } from "hannes-ui/components";
+
 export default async function Dashboard() {
   const supabase = createServerSupabaseClient();
   const {
@@ -14,6 +18,16 @@ export default async function Dashboard() {
     <div className="">
       {/* <h1 className="text-3xl font-bold mb-2">Välkommen {user.email} 👋</h1> */}
       <Map />
+
+      {/* <Button
+        label="Click me!"
+        variant="solid"
+        onClick={() => console.log("clicked")}
+      /> */}
+      <div>
+        <h2>External Paketets TestComponent</h2>
+        <Badge>Hej från External!</Badge>
+      </div>
     </div>
   );
 }
